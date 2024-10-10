9ea0c1ee90d73eb91ee81ade09c3b238a7b0e308
RizzoMason <badrdelif@gmail.com>
2024-10-10 01:54:17 +0100

    ss

--- src/pages/articles/Articles.js
+++ src/pages/articles/Articles.js
@@ -1,16 +1,3 @@
-import { useState, useEffect } from 'react';
-import { formatDate } from 'utils/date';
-
-export const ArticlesPost = ({ date }) => {
-  const [dateTime, setDateTime] = useState(null);
-
-  useEffect(() => {
-    setDateTime(formatDate(date));
-  }, [date]);
-
-  return (
-    <div>
-      <p>{dateTime}</p>
-    </div>
-  );
+export default function ArticlesPost() {
+  return <>{/* nothing */}</>;
 };

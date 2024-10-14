a6d47e4a3d076d707f84190c8fd0bb41958658b5
RizzoMason <badrdelif@gmail.com>
2024-10-14 19:04:48 +0100

    Update _app.page.js

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -18,10 +18,7 @@ import { ScrollRestore } from '../layouts/App/ScrollRestore';
 export const AppContext = createContext({});
 
 const repoPrompt = `
-__  __  __
-\u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215
-\n\nTaking a peek huh? Check out the source code: https://github.com/tresillo2017/portfolio-1
-`;
+Made by Rami Badr.`;
 
 const App = ({ Component, pageProps }) => {
   const [storedTheme] = useLocalStorage('theme', 'dark');
